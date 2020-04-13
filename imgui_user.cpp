c31ab08df0f86bce51b658c351574f0a03ba0f72
ikrima <github@ikrima.com>
2020-04-13 03:47:17 -0700

    More splits of imgui_user.h/cpp

--- imgui_user.cpp
+++ imgui_user.cpp
@@ -1,24 +1,132 @@
 // Copyright Kite & Lightning
 
-#include "imgui_user.h"
 #include "imgui.h"
 
+//========================================================================================================================
+#pragma region Support for global across dll boundaries ImGui context
 #if IMGUIUX_SHAREDLIB
+namespace { ImGuiContext* globalImGuiCtx = nullptr; }
+ImGuiContext* ImGuiUX::GetGlobalImGuiCtx() { return globalImGuiCtx; }
+void ImGuiUX::SetGlobalImGuiCtx(ImGuiContext* InCtx) { globalImGuiCtx = InCtx; }
+#endif
+#pragma endregion
+//========================================================================================================================
 
-namespace
-{
-    ImGuiContext* globalImGuiCtx = nullptr;
+//========================================================================================================================
+#pragma region Scoped Helper Utils
 
-}
+#define IM_CONCAT(x, y) IM_CONCAT_IMPL(x, y)
+#define IM_CONCAT_IMPL(x, y) x##y
+#define IMWITH(x) if(auto IM_CONCAT(hold,__LINE__) = ImGuiUX::##x; IM_CONCAT(hold,__LINE__).IsOpen())
 
-ImGuiContext* ImGuiUX::GetGlobalImGuiCtx()
-{
-    return globalImGuiCtx;
-}
 
-void ImGuiUX::SetGlobalImGuiCtx(ImGuiContext* InCtx)
-{
-    globalImGuiCtx = InCtx;
-}
+namespace ImGuiUX {
+
+    struct [[nodiscard]] _Scope
+    {
+        _Scope() = default;
+        _Scope(const _Scope&) = delete;
+        _Scope& operator=(const _Scope&) = delete;
+
+        bool IsOpen() const
+        {
+            return _bOpened;
+        }
+
+    protected:
+        bool _bOpened = true;
+    };
+
+
+
+    //------------------------------------------------------------------------------------------------------------------------
+#pragma region ImGui Scope Helpers
+    struct [[nodiscard]] ImScopeIndent final : _Scope
+    {
+        ImScopeIndent() { ImGui::Indent(); }
+        ~ImScopeIndent() { ImGui::Unindent(); }
+    };
+
+    struct [[nodiscard]] ImScopeWindow final : _Scope
+    {
+        ImScopeWindow(
+            const char* name,
+            bool* p_open = nullptr,
+            ImGuiWindowFlags flags = 0,
+            const char* dockSpaceName = nullptr)
+        {
+            if (dockSpaceName)
+            {
+                const ImGuiID HostDockspaceId = ImGui::GetID(dockSpaceName);
+                ImGui::SetNextWindowDockID(HostDockspaceId, ImGuiCond_FirstUseEver);
+            }
+            _bOpened = ImGui::Begin(name, p_open, flags);
+        }
+
+        ~ImScopeWindow()
+        {
+            // Always call End
+            ImGui::End();
+        }
+    };
+
+    struct [[nodiscard]] ImScopeMainMenuBar final : _Scope
+    {
+        ImScopeMainMenuBar() { _bOpened = ImGui::BeginMainMenuBar(); }
+        ~ImScopeMainMenuBar() { if (_bOpened) { ImGui::EndMainMenuBar(); } }
+    };
+
+    struct [[nodiscard]] ImScopeMenu final : _Scope
+    {
+        ImScopeMenu(const char* name) { _bOpened = ImGui::BeginMenu(name); }
+        ~ImScopeMenu() { if (_bOpened) { ImGui::EndMenu(); } }
+    };
+
+    struct [[nodiscard]] ImScopePopup final : _Scope
+    {
+        ImScopePopup(const char* str_id, ImGuiWindowFlags flags = 0) { _bOpened = ImGui::BeginPopup(str_id,flags); }
+        ~ImScopePopup() { if (_bOpened) { ImGui::EndPopup(); } }
+    };
+#pragma endregion
+    //------------------------------------------------------------------------------------------------------------------------
+
+
+    //------------------------------------------------------------------------------------------------------------------------
+#pragma region Node Editor Scope Helpers
+#if IMGUIUIX_FEATURE_NODEDITOR
+
+    namespace ned = ax::NodeEditor;
+    struct [[nodiscard]] ImScopeNed final : _Scope
+    {
+        ImScopeNed(const char* id) { ned::Begin(id); }
+        ~ImScopeNed() { ned::End(); }
+    };
+
+    struct [[nodiscard]] ImScopeNedNode final : _Scope
+    {
+        ImScopeNedNode(ned::NodeId id) { ned::BeginNode(id); }
+        ~ImScopeNedNode() { ned::EndNode(); }
+    };
+
+    struct [[nodiscard]] ImScopeNedCreate final : _Scope
+    {
+        ImScopeNedCreate(const ImVec4 & color = ImVec4(1, 1, 1, 1), float thickness = 1.0f) { _bOpened = ned::BeginCreate(color,thickness); }
+        ~ImScopeNedCreate() { ned::EndCreate(); }
+    };
+
+    struct [[nodiscard]] ImScopeNedDelete final : _Scope
+    {
+        ImScopeNedDelete() { _bOpened = ned::BeginDelete(); }
+        ~ImScopeNedDelete() { ned::EndDelete(); }
+    };
 
 #endif
+#pragma endregion
+    //------------------------------------------------------------------------------------------------------------------------
+
+
+}
+
+
+#pragma endregion
+//========================================================================================================================
--- imgui_user.h
+++ imgui_user.h
@@ -1,20 +1,8 @@
 #pragma once
 
-
-
-//========================================================================================================================
-#pragma region Widget Extensions
-
-#if IMGUIUIX_FEATURE_ADOBESPECTRUM
-#include "ImGuiUX/AdobeSpectrum/spectrum.h"
+#ifndef IMGUI_VERSION
+#error Must include imgui.h before imgui_internal.h
 #endif
-
-#if IMGUIUIX_FEATURE_NODEDITOR
-#include "ImGuiUX/DmdNodeEditor/NodeEditor/include/imgui_node_editor.h"
-#endif
-
-#pragma endregion
-//========================================================================================================================
 
 //========================================================================================================================
 #pragma region Support for global across dll boundaries ImGui context
@@ -34,9 +22,22 @@ namespace ImGuiUX {
 
 
 //========================================================================================================================
+#pragma region Widget Extensions
+
+#if IMGUIUIX_FEATURE_ADOBESPECTRUM
+#include "ImGuiUX/AdobeSpectrum/spectrum.h"
+#endif
+
+#if IMGUIUIX_FEATURE_NODEDITOR
+#include "ImGuiUX/DmdNodeEditor/NodeEditor/include/imgui_node_editor.h"
+#endif
+
+#pragma endregion
+//========================================================================================================================
+
+
+//========================================================================================================================
 #pragma region Scoped Helper Utils
-
-
 
 #define IM_CONCAT(x, y) IM_CONCAT_IMPL(x, y)
 #define IM_CONCAT_IMPL(x, y) x##y
@@ -45,71 +46,16 @@ namespace ImGuiUX {
 
 namespace ImGuiUX {
 
-    struct [[nodiscard]] _Scope
-    {
-        _Scope() = default;
-        _Scope(const _Scope&) = delete;
-        _Scope& operator=(const _Scope&) = delete;
-
-        bool IsOpen() const
-        {
-            return _bOpened;
-        }
-
-    protected:
-        bool _bOpened = true;
-    };
-
+    struct [[nodiscard]] _Scope;
 
 
 //------------------------------------------------------------------------------------------------------------------------
 #pragma region ImGui Scope Helpers
-    struct [[nodiscard]] ImScopeIndent final : _Scope
-    {
-        ImScopeIndent() { ImGui::Indent(); }
-        ~ImScopeIndent() { ImGui::Unindent(); }
-    };
-
-    struct [[nodiscard]] ImScopeWindow final : _Scope
-    {
-        ImScopeWindow(
-            const char* name,
-            bool* p_open = nullptr,
-            ImGuiWindowFlags flags = 0,
-            const char* dockSpaceName = nullptr)
-        {
-            if (dockSpaceName)
-            {
-                const ImGuiID HostDockspaceId = ImGui::GetID(dockSpaceName);
-                ImGui::SetNextWindowDockID(HostDockspaceId, ImGuiCond_FirstUseEver);
-            }
-            _bOpened = ImGui::Begin(name, p_open, flags);
-        }
-
-        ~ImScopeWindow()
-        {
-            // Always call End
-            ImGui::End();
-        }
-    };
-
-    struct [[nodiscard]] ImScopeMainMenuBar final : _Scope
-    {
-        ImScopeMainMenuBar() { _bOpened = ImGui::BeginMainMenuBar(); }
-        ~ImScopeMainMenuBar() { if (_bOpened) { ImGui::EndMainMenuBar(); } }
-    };
-
-    struct [[nodiscard]] ImScopeMenu final : _Scope
-    {
-        ImScopeMenu(const char* name) { _bOpened = ImGui::BeginMenu(name); }
-        ~ImScopeMenu()                { if (_bOpened) { ImGui::EndMenu(); } }
-    };
-
-    struct [[nodiscard]] ImScopePopup final : _Scope
-    {
-        ImScopePopup(const char* str_id, ImGuiWindowFlags flags=0) { _bOpened = ImGui::BeginPopup(str_id,flags); }
-        ~ImScopePopup()                                            { if (_bOpened) { ImGui::EndPopup(); } }
-    };
+    struct [[nodiscard]] ImScopeIndent;
+    struct [[nodiscard]] ImScopeWindow;
+    struct [[nodiscard]] ImScopeMainMenuBar;
+    struct [[nodiscard]] ImScopeMenu;
+    struct [[nodiscard]] ImScopePopup;
 #pragma endregion
 //------------------------------------------------------------------------------------------------------------------------
 
@@ -118,30 +64,10 @@ namespace ImGuiUX {
 #pragma region Node Editor Scope Helpers
 #if IMGUIUIX_FEATURE_NODEDITOR
 
-    namespace ned = ax::NodeEditor;
-    struct [[nodiscard]] ImScopeNed final : _Scope
-    {
-        ImScopeNed(const char* id) { ned::Begin(id); }
-        ~ImScopeNed() { ned::End(); }
-    };
-
-    struct [[nodiscard]] ImScopeNedNode final : _Scope
-    {
-        ImScopeNedNode(ned::NodeId id) { ned::BeginNode(id); }
-        ~ImScopeNedNode()              { ned::EndNode();     }
-    };
-
-    struct [[nodiscard]] ImScopeNedCreate final : _Scope
-    {
-        ImScopeNedCreate(const ImVec4& color = ImVec4(1, 1, 1, 1), float thickness = 1.0f)  { _bOpened = ned::BeginCreate(color,thickness); }
-        ~ImScopeNedCreate() { ned::EndCreate(); }
-    };
-
-    struct [[nodiscard]] ImScopeNedDelete final : _Scope
-    {
-        ImScopeNedDelete()  { _bOpened = ned::BeginDelete(); }
-        ~ImScopeNedDelete() { ned::EndDelete(); }
-    };
+    struct [[nodiscard]] ImScopeNed;
+    struct [[nodiscard]] ImScopeNedNode;
+    struct [[nodiscard]] ImScopeNedCreate ;
+    struct [[nodiscard]] ImScopeNedDelete ;
 
 #endif
 #pragma endregion
