fbccbc4d0626893f0dc3d6a21b85b19fe08701c5
ikrima <contact@ikrima.com>
2020-04-21 09:32:51 -0700

    Minor linting fix

--- ImGuiUX/imguix.cpp
+++ ImGuiUX/imguix.cpp
@@ -20,5 +20,5 @@ ImGuiViewport* ImGuiUX::FindViewportByPlatformHandleRaw(void* platform_handle_ra
     for (int i = 0; i != g.Viewports.Size; i++)
         if (g.Viewports[i]->PlatformHandleRaw == platform_handle_raw)
             return g.Viewports[i];
-    return NULL;
+    return nullptr;
 };
