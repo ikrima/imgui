8135f998115c3ddaf9cb1fcd945637a13e0c79da
ikrima <contact@ikrima.com>
2021-02-13 18:40:52 -0800

    feat: allow define override of IMGUI_DEBUG_*

--- imgui_internal.h
+++ imgui_internal.h
@@ -175,10 +175,18 @@ namespace ImStb
 //#define IMGUI_DEBUG_LOG_VIEWPORT      IMGUI_DEBUG_LOG // Enable log
 //#define IMGUI_DEBUG_LOG_DOCKING       IMGUI_DEBUG_LOG // Enable log
 //#define IMGUI_DEBUG_LOG_NAV           IMGUI_DEBUG_LOG // Enable log
+#ifndef IMGUI_DEBUG_LOG_POPUP
 #define IMGUI_DEBUG_LOG_POPUP(...)      ((void)0)       // Disable log
+#endif
+#ifndef IMGUI_DEBUG_LOG_NAV
 #define IMGUI_DEBUG_LOG_NAV(...)        ((void)0)       // Disable log
+#endif
+#ifndef IMGUI_DEBUG_LOG_VIEWPORT
 #define IMGUI_DEBUG_LOG_VIEWPORT(...)   ((void)0)       // Disable log
+#endif
+#ifndef IMGUI_DEBUG_LOG_DOCKING
 #define IMGUI_DEBUG_LOG_DOCKING(...)    ((void)0)       // Disable log
+#endif
 
 // Static Asserts
 #if (__cplusplus >= 201100)
